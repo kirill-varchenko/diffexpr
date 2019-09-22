dac3fa5a2bcdf72e13e6a2c357f4e791a409b93b
Kirill Varchenko <kirill.varchenko@gmail.com>
2019-09-22 18:12:06 +0300

    Added converters for key/value in FileDict class to cast data from file.

--- FileDict.py
+++ FileDict.py
@@ -3,19 +3,23 @@ import re
 
 class FileDict(dict):
     """Associate dict with file and given regex pattern with 'key' and 'value' named groups"""
-    def __init__(self, file, pattern):
+
+    def __init__(self, file, pattern, key_converter=None, value_converter=None):
         super().__init__()
         self.file = file
         self.pattern = pattern
         self.re_comp = re.compile(pattern)
+        self.key_converter = key_converter
+        self.value_converter = value_converter
 
     def preload(self, *keys):
         """Preload items with given keys from file to dict"""
-        set_keys = {str(k) for k in keys}
+        set_keys = set(keys)
         with open(self.file, 'r') as fi:
             for line in fi:
                 m = self.re_comp.search(line)
-                key, value = m.group('key'), m.group('value')
+                key = m.group('key') if self.key_converter is None else self.key_converter(m.group('key'))
+                value = m.group('value') if self.value_converter is None else self.value_converter(m.group('value'))
                 if key in set_keys:
                     set_keys.remove(key)
                     self[key] = value
